478bd6e7e0d29d37ffc88e4a4837454c781b5c66
Stanislav Samsonenko <samsonenko2002@yandex.ru>
2022-10-14 17:23:10 +0300

    Added Third practical work

---
+++ pract3/apache-php/src/goods.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Каталог</title>
+    <style>
+        span {
+            margin: 10px;
+        }
+        .list {
+            display: flex;
+            flex-direction: column;
+        }
+        .item {
+            display: flex;
+            flex-direction: row;
+            cursor: pointer;
+            text-decoration: underline;
+            color: blue;
+        }
+
+        .item:hover { background-color: cadetblue; color: blueviolet }
+        </style>
+
+</head>
+<body>
+<h1>Каталог</h1>
+<?php
+require_once 'helper.php';
+$mysqli = openMysqli();
+$result = $mysqli->query("select * from " . goods);
+?>
+<div class="list"><?php if ($result->num_rows > 0) foreach ($result as $good) echo <<<A
+            <div
+                onclick="window.location.replace('view.php?id={$good[id]}')" 
+                class="item">
+                <span>{$good[title]}</span>
+            </div>
+        A; else echo 'Empty'; ?></div>
+</body>
+</html>
---
+++ pract3/apache-php/src/helper.php
@@ -0,0 +1,20 @@
+<?php
+const
+host = 'db',
+users = 'users',
+name = 'name',
+dbUser = 'user',
+password = 'password',
+db = 'appDb',
+goods = 'goods',
+id = 'ID',
+title = 'title',
+description = 'description',
+cost = 'cost';
+
+function openMysqli(): mysqli
+{
+    return new mysqli(
+        host, dbUser, password, db
+    );
+}
